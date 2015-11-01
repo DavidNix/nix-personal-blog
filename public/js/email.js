f516a3ddb2c9909301b58d91b55d3844f9f34146
David Nix <davidnix81@gmail.com>
2015-11-01 10:08:12 -0700

    RELEASE Sun Nov  1 10:08:12 MST 2015

---
+++ public/js/email.js
@@ -0,0 +1,16 @@
+window.obfuscate = function (selector) {
+  var $el = $(selector);
+  var p = "mailto:";
+  var n = "hello";
+  var d = "davidnix";
+  var href = p + n + "@" + d + ".io";
+
+  $el.on("click", function () {
+    $el.attr("href", href);
+    setTimeout(function () {
+      $el.attr("href", "#");
+    }, 25);
+  });
+};
+
+obfuscate("#email");
